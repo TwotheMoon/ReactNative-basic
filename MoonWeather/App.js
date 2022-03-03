27a2642be395ed46501e5b3cdc6539a65f91d6ac
2theMoon <dlaguddh1@gmail.com>
2022-03-03 16:23:29 +0900

    PJ 생성

---
+++ MoonWeather/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>test</Text>
+      <Text></Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 28,
+    color: "white",
+  }
+});
